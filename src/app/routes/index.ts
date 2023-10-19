ee26f31d1cb3076d3d95c75b21880c1216da4c4c
Noormohammad011 <noormohammad.net1503011@gmail.com>
2023-10-19 15:18:40 +0600

    'admin-module'

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { AuthRoutes } from '../modules/auth/auth.routes';
 import { UserRoutes } from '../modules/user/user.routes';
+import { AdminRoutes } from '../modules/admin/admin.routes';
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ const moduleroutes = [
     {
         path: '/users',
         route: UserRoutes,
+    },
+    {
+        path: '/admins',
+        route: AdminRoutes,
     },
 ];
 
